1180c7d875bdbec6fc5c1dc63d319ba2bd4af1b9
r.tohidifar <r.tohidifar@N-RTOHIDIFAR>
2023-10-28 12:26:59 +0330

    Add API and Swagger

---
+++ APIForCRA/MobileCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIForCRA
+{
+    public class MobileCount
+    {
+        public string identificationNo { get; set; }
+        public int identificationType { get; set; }
+    }
+}
