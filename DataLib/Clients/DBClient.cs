e5133b5b993ffcb0d6505228bfb0db81e3c26937
r.tohidifar <r.tohidifar@N-RTOHIDIFAR>
2023-10-29 10:10:30 +0330

    edit new pproject

--- DataLib/Clients/DBClient.cs
+++ DataLib/Clients/DBClient.cs
@@ -1,12 +1,4 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration.Json;
 
 namespace DataLib
 {
